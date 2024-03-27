27cba42c764bbb9fe2addc4e2a569894869a6ef0
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-03-27 12:10:02 +0900

    Title: 배열 채우기, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/배열 채우기/filling-array.py
+++ 240327/배열 채우기/filling-array.py
@@ -1,5 +1,11 @@
 arr = list(map(int,input().split()))
+ans=[]
 
-for i in arr[::-1]:
+for i in arr:
     if i!=0:
-        print(i,end=" ")
+        ans.append(i)
+    elif i==0:
+        break
+
+for j in ans[::-1]:
+    print(j,end=" ")

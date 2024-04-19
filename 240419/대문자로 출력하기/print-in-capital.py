5afb075d17ac1a5dc05a986c66c82d3cf149ce5c
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-04-19 15:47:57 +0900

    Title: 대문자로 출력하기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/대문자로 출력하기/print-in-capital.py
+++ 240419/대문자로 출력하기/print-in-capital.py
@@ -3,4 +3,9 @@ lght=len(x)
 
 for i in range(lght):
     if x[i].isalpha():
-        print(x[i].upper(),end="")
+        if 'a'<=x[i] and x[i]<='z':
+            cap=ord('A')-ord('a')
+            ascii=ord(x[i])+cap
+            print(chr(ascii),end="")
+        else:
+            print(x[i],end="")

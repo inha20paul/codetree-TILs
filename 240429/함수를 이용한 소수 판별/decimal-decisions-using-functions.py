d61627b946c801d2348e780c7245ba4a727de484
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-04-29 22:47:22 +0900

    Title: 함수를 이용한 소수 판별, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240429/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240429/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,8 +1,9 @@
 def isPrime(n):
-    for i in range(2,n):
+    cnt=0
+    for i in range(1,n+1):
         if n%i==0:
-            return False
-    return True
+            cnt+=1
+    return cnt==2
 arr=[0]
 a,b=map(int,input().split())
 for j in range(a,b+1):

c6a792efaed8124f0f619a08a34bae70795b31b9
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-04-30 20:49:16 +0900

    Title: 1이 3개 이상 있는 위치, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240430/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,7 +1,7 @@
 # 격자받기
 n=int(input())
 arr=[list(map(int,input().split())) for _ in range(n)]
-cnt=0
+ans=0
 # 동서남북
 dxs=[1,-1,0,0]
 dys=[0,0,-1,1]
@@ -9,16 +9,16 @@ dys=[0,0,-1,1]
 def in_range(x,y):
     return 0<=x<=n-1 and 0<=y<=n-1
 
-for i in range(n):
-    for j in range(n):
-        num=0
+for x in range(n):
+    for y in range(n):
+        cnt=0
         for dx,dy in zip(dxs,dys):
-            x,y=i,j
-            x+=dx
-            y+=dy
-            if in_range(x,y):
-                num+=arr[x][y]
-        if num>=3:
-            cnt+=1
+            nx=x+dx
+            ny=y+dy
+            if not in_range(nx,ny):
+                continue
+            cnt+=arr[nx][ny]
+        if cnt>=3:
+            ans+=1
 
-print(cnt)
+print(ans)

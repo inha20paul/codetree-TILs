fda8fcf7c8fca1f44c8f0a1a64c844736d00f6de
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-04-30 19:55:09 +0900

    Title: 문자에 따른 명령 2, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240430/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,25 @@
+string=input()
+
+
+#시작위치
+x,y=0,0
+
+# 시작방향=북
+dir_num=0
+# 북,동,남,서
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
+
+for i in string:
+    # 반시계 회전
+    if i=='L':
+        dir_num=(4+dir_num-1)%4
+    # 시계방향 회전
+    elif i=='R':
+        dir_num=(dir_num+1)%4
+    # 전진
+    elif i=='F':
+        x+=dx[dir_num]
+        y+=dy[dir_num]
+
+print(x,y)

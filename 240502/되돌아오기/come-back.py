c1724df2d372c4606d83f7f7a18ac87785d14bb2
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-02 00:24:24 +0900

    Title: 되돌아오기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/되돌아오기/come-back.py
@@ -0,0 +1,24 @@
+N=int(input())
+cnt=0
+x,y=0,0
+mapping={
+    'E':0,
+    'W':1,
+    'S':2,
+    'N':3
+}
+# 동,서,남,북
+dx=[1,-1,0,0]
+dy=[0,0,-1,1]
+
+for i in range(N):
+    way,num=input().split()
+    num=int(num)
+    direction=mapping[way]
+    for j in range(num):
+        x+=dx[direction]
+        y+=dy[direction]
+        cnt+=1
+        if x==0 and y==0:
+            print(cnt)
+            break

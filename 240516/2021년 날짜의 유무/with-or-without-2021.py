5391601e087dbfa7228c06b4d84506d8f3d42eae
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-16 15:58:10 +0900

    Title: 2021년 날짜의 유무, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240516/2021년 날짜의 유무/with-or-without-2021.py
+++ 240516/2021년 날짜의 유무/with-or-without-2021.py
@@ -5,11 +5,11 @@ M,D=map(int,input().split())
 def IsMD(M,D):
     arr31=[1,3,5,7,8,10,12]
     if M in arr31:
-        return 1<=D<=31
+        return 1<=M<=12 and 1<=D<=31
     elif M==2: # D는 28일까지
-        return 1<=D<=28
+        return 1<=M<=12 and 1<=D<=28
     else: # 30일까지
-        return 1<=D<=30
+        return 1<=M<=12 and 1<=D<=30
 
 if IsMD(M,D):
     print("Yes")

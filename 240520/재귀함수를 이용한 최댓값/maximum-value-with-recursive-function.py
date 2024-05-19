2ec632ba3fd2c774487b6d51cf101119ab64facb
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-20 00:10:21 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240520/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,13 +1,9 @@
 n=int(input())
 arr=list(map(int,input().split()))
-ans=0
-def f(n):
-    global ans
-    if n==0:
-        return
-    if ans<=arr[n-1]:
-        ans=arr[n-1]
-    f(n-1)
 
-f(n)
-print(ans)
+def f(n):
+    if n==0:
+        return arr[0]
+    return max(arr[n-1],f(n-1))
+
+print(f(n))

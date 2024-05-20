8f16c5b28559d35bfe3214fa7ff9853253e773cc
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-20 20:46:33 +0900

    Title: 계단 오르기, Time: 104ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240520/계단 오르기/climbing-stairs.py
@@ -0,0 +1,13 @@
+n=int(input())
+memo=[0]*(n+1)
+
+def f(n):
+    if n==1 or n==-1:
+        return 0
+    if n==0:
+        return 1
+    if memo[n]==0:
+        memo[n]=f(n-2)+f(n-3)
+    return memo[n]
+
+print(f(n))

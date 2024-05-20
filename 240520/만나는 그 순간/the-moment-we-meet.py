16405d47f56ba22aa02c445c1cce925fbfbebbf7
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-20 21:33:37 +0900

    Title: 만나는 그 순간, Time: 216ms, Memory: 42MB, Status: Passed - CodeTree

--- 240520/만나는 그 순간/the-moment-we-meet.py
+++ 240520/만나는 그 순간/the-moment-we-meet.py
@@ -1,15 +1,21 @@
 N,M=map(int,input().split())
-arr=[0]
+# A 위치 기록
+arrA=[0]
 # A의 현재위치
 A=0
+
+# B 위치 기록
+arrB=[0]
 # B의 현재위치
 B=0
+
 dx={
     'L':-1,
     'R':1
 }
-ans=0
-cnt=0
+
+ans=-1
+
 
 for i in range(N):
     d,t=input().split()
@@ -17,18 +23,19 @@ for i in range(N):
     for j in range(t):
         A+=dx[d]
         # t초일때 A의 위치 저장
-        arr.append(A)
+        arrA.append(A)
 
 for i in range(M):
-    if ans!=0:
-        break
     d,t=input().split()
     t=int(t)
     for j in range(t):
         B+=dx[d]
-        cnt+=1
-        if arr[cnt]==B:
-            ans=cnt
-            break
+        # t초일때 A의 위치 저장
+        arrB.append(B)
+
+for i in range(1,len(arrA)):
+    if arrA[i]==arrB[i]:
+        ans=i
+        break
 
 print(ans)

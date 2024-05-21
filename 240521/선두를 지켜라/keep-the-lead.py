379975d0a7b25235a9135d803bc3e7fbd0e0f8ba
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-21 18:39:53 +0900

    Title: 선두를 지켜라, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240521/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,31 @@
+#A,B 각각 이동 횟수?
+N,M=map(int,input().split())
+A=0
+arrA=[]
+B=0
+arrB=[]
+
+for i in range(N):
+    v,t=map(int,input().split())
+    for j in range(t):
+        A+=v
+        arrA.append(A)
+
+for i in range(M):
+    v,t=map(int,input().split())
+    for j in range(t):
+        B+=v
+        arrB.append(B)
+
+state=''
+cnt=0
+for i in range(len(arrA)):
+    prev=state
+    if arrA[i]>arrB[i]:
+        state='A'
+    elif arrA[i]<arrB[i]:
+        state='B'
+    if state!=prev:
+        cnt+=1
+
+print(cnt-1)

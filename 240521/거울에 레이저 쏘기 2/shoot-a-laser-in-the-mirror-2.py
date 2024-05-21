ab97990935910b7c85b32001c1e8ee3e67489e29
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-21 23:14:02 +0900

    Title: 거울에 레이저 쏘기 2, Time: 164ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240521/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,55 @@
+N=int(input())
+arr=[input() for _ in range(N)]
+k=int(input())
+
+d=(k-1)//N
+
+if d==0:
+    r=0
+    c=(k-1)%N
+elif d==2:
+    r=N-1
+    c=(N-1)-(k-1)%N
+elif d==3:
+    r=(N-1)-(k-1)%N
+    c=0
+else:
+    r=(k-1)%N
+    c=N-1
+
+
+def inRange(r,c):
+    return 0<=r<=N-1 and 0<=c<=N-1
+
+# \ 방향전환
+m={
+    0:1,
+    1:0,
+    2:3,
+    3:2
+}
+
+# 방향 반전
+back={
+    0:2,
+    1:3,
+    2:0,
+    3:1
+}
+
+def nextP(r,c,dirr):
+    if not inRange(r,c):
+        return 0
+    # 상,우,하,좌
+    dc=[0,1,0,-1]
+    dr=[-1,0,1,0]
+    if arr[r][c]=='/':
+        # 방향 전환
+        dirr=(3-dirr)%4
+    else:
+        dirr=m[dirr]
+    nr,nc=r+dr[dirr],c+dc[dirr]
+    dirr=back[dirr]
+    return nextP(nr,nc,dirr)+1
+
+print(nextP(r,c,d))

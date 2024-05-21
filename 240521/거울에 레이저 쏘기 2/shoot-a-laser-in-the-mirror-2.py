5c13e3eed5782da3d1e4e6eb8cb131a1afff4d93
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-21 23:19:38 +0900

    Title: 거울에 레이저 쏘기 2, Time: 229ms, Memory: 25MB, Status: Passed - CodeTree

--- 240521/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240521/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,9 +1,10 @@
+# 입력부
 N=int(input())
 arr=[input() for _ in range(N)]
 k=int(input())
 
-d=(k-1)//N
-
+# 초기위치 r,c
+d=(k-1)//N # 초기 입력 방향 (상우하좌)
 if d==0:
     r=0
     c=(k-1)%N
@@ -17,11 +18,15 @@ else:
     r=(k-1)%N
     c=N-1
 
-
+# 격자범위
 def inRange(r,c):
     return 0<=r<=N-1 and 0<=c<=N-1
 
-# \ 방향전환
+# 상,우,하,좌
+dc=[0,1,0,-1]
+dr=[-1,0,1,0]
+
+# \모양거울 방향전환
 m={
     0:1,
     1:0,
@@ -38,18 +43,17 @@ back={
 }
 
 def nextP(r,c,dirr):
+    # 종료조건
     if not inRange(r,c):
         return 0
-    # 상,우,하,좌
-    dc=[0,1,0,-1]
-    dr=[-1,0,1,0]
-    if arr[r][c]=='/':
+    if arr[r][c]=='/':  #'/'거울
         # 방향 전환
         dirr=(3-dirr)%4
-    else:
+    else: #'\' 거울
+        # 방향전환
         dirr=m[dirr]
-    nr,nc=r+dr[dirr],c+dc[dirr]
-    dirr=back[dirr]
+    nr,nc=r+dr[dirr],c+dc[dirr] # 다음격자
+    dirr=back[dirr] # 방향 반전
     return nextP(nr,nc,dirr)+1
 
 print(nextP(r,c,d))

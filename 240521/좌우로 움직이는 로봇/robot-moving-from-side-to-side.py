03bb1f8a2e1e0b3974bf2b0c30306750211cde4c
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-21 19:40:05 +0900

    Title: 좌우로 움직이는 로봇, Time: 355ms, Memory: 63MB, Status: Passed - CodeTree

--- 240521/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240521/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -26,10 +26,26 @@ for i in range(m):
     
 cnt=0
 check=max(len(arrA),len(arrB))
+limit=min(len(arrA),len(arrB))-1
 
 for i in range(1,check):
-    num=min(len(arrA)-1,i)
-    if arrA[num]==arrB[i] and arrA[num-1]!=arrB[i-1]:
-        cnt+=1
+    if len(arrA)<=len(arrB):
+        if i<=limit:
+            if arrA[i]==arrB[i] and arrA[i-1]!=arrB[i-1]:
+                cnt+=1
+                
+        else:
+            if arrA[limit]==arrB[i]:
+                cnt+=1
+                
+    else:
+        if i<=limit:
+            if arrA[i]==arrB[i] and arrA[i-1]!=arrB[i-1]:
+                cnt+=1
+            
+        else:
+            if arrA[i]==arrB[limit]:
+                cnt+=1
+                
 
 print(cnt)

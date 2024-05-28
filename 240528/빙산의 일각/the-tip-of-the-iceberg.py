802fabebc5b37814c0a1a9024c3493e9db8271b2
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-28 23:39:50 +0900

    Title: 빙산의 일각, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240528/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,15 +1,18 @@
 N=int(input())
 H=[-1]
+idx=[]
 ans=0
 cnt=0
 for i in range(N):
     h=int(input())
     H.append(h)
+    idx.append((h,i))
 H.append(-1)
 
+idx.sort(key=lambda x:x[0])
 
-for i in range(1,len(H)):
-    mx_idx=H.index(max(H))
+for tup in idx:
+    mx_idx=tup[1]
     if H[mx_idx-1]!=0 and H[mx_idx+1]!=0:
         cnt+=1
     elif H[mx_idx-1]==0 and H[mx_idx+1]==0:

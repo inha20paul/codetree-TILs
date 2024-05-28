cba1a64d8222ab8cede76c6d35cb1964bde84bf1
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-28 15:47:03 +0900

    Title: 빙산의 일각, Time: 5059ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240528/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240528/빙산의 일각/the-tip-of-the-iceberg.py
@@ -8,20 +8,34 @@ for i in range(N):
     if h not in m:
         m.append(h)
 
+# def count_ice(height):
+#     cnt=0
+#     switch=False
+#     arr=[max(0,elem-height) for elem in H]
+#     for i in range(N):
+#         if not switch and arr[i]!=0:
+#             cnt+=1
+#             switch=True
+#         elif arr[i]==0:
+#             switch=False
+#     return cnt
+
+def isHigh(a,b):
+    if a-b>0:
+        return 1
+    else:
+        return 0
+
 def count_ice(height):
     cnt=0
-    switch=False
-    arr=[max(0,elem-height) for elem in H]
-    for i in range(N):
-        if not switch and arr[i]!=0:
-            cnt+=1
-            switch=True
-        elif arr[i]==0:
-            switch=False
-    return cnt
+    arr=[isHigh(H[0],height)]
+    for i in range(1,N):
+        n=isHigh(H[i],height)
+        if arr[-1]!=n:
+            arr.append(n)
+    return sum(arr)
 
 ans=0
-
 for i in m:
     ans=max(ans,count_ice(i))
 

e95d66930bbea8512854a5f46f8222027df17a25
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-28 15:17:39 +0900

    Title: 빙산의 일각, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240528/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,12 +1,26 @@
 N=int(input())
 H=[]
-m=[]
+# m=[]
+diff=[]
+diff_cnt=[]
 
-for i in range(N):
+h=int(input())
+H.append(h)
+
+for i in range(N-1):
     h=int(input())
     H.append(h)
-    if h not in m:
-        m.append(h)
+    if h-H[i]>0:
+        if H[i] not in diff:
+            diff.append(H[i])
+            diff_cnt.append(0)
+        else:
+            idx=diff.index(H[i])
+            diff_cnt[idx]+=1
+
+
+    # if h not in m:
+    #     m.append(h)
 
 def count_ice(height):
     cnt=0
@@ -20,9 +34,16 @@ def count_ice(height):
             switch=False
     return cnt
 
+# ans=0
+
+# for i in m:
+#     ans=max(ans,count_ice(i))
+
+# print(ans)
 ans=0
+ans_idx=0
+for i in range(len(diff_cnt)):
+    if diff_cnt[i]>ans:
+        ans_idx=i
 
-for i in m:
-    ans=max(ans,count_ice(i))
-
-print(ans)
+print(count_ice(diff[ans_idx]))

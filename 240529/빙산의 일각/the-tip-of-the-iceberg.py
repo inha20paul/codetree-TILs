a3e7ac449d06cd6e9c9f71430855413ba31ebbcc
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-29 00:23:03 +0900

    Title: 빙산의 일각, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240529/빙산의 일각/the-tip-of-the-iceberg.py
@@ -0,0 +1,27 @@
+N=int(input())
+H=[-1]
+idx=[]
+ans=0
+cnt=0
+for i in range(N):
+    h=int(input())
+    #높이 같은 문제 해결
+    if H[i-1]!=h:
+        H.append(h)
+        idx.append((h,i))
+H.append(-1)
+
+idx.sort(key=lambda x:x[0],reverse=True)
+
+
+for tup in idx:
+    mx_idx=tup[1]+1
+    if H[mx_idx-1]!=0 and H[mx_idx+1]!=0:
+        cnt+=1
+    elif H[mx_idx-1]==0 and H[mx_idx+1]==0:
+        cnt-=1
+    # 방문 체크
+    H[mx_idx]=0
+    ans=max(cnt,ans)
+    
+print(ans)

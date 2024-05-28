e1a01e306b1b3aceff38e3829379cfc967a1e54b
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-29 00:33:34 +0900

    Title: 빙산의 일각, Time: 490ms, Memory: 31MB, Status: Passed - CodeTree

--- 240529/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240529/빙산의 일각/the-tip-of-the-iceberg.py
@@ -2,16 +2,19 @@ N=int(input())
 H=[-1]
 idx=[]
 ans=0
+indices=0
 cnt=0
 for i in range(N):
     h=int(input())
     #높이 같은 문제 해결
     if H[-1]!=h:
         H.append(h)
-        idx.append((h,H.index(h)))
+        indices+=1
+        idx.append((h,indices))
 H.append(-1)
-
 idx.sort(key=lambda x:x[0],reverse=True)
+
+
 
 for tup in idx:
     mx_idx=tup[1]

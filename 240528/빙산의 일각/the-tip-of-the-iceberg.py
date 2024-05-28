a410ffb44e0a5c4d05cfbdd36ae625a73e88bdbd
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-28 17:05:27 +0900

    Title: 빙산의 일각, Time: 5097ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240528/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240528/빙산의 일각/the-tip-of-the-iceberg.py
@@ -1,43 +1,21 @@
 N=int(input())
-H=[]
-m=[]
-
+H=[-1]
+ans=0
+cnt=0
 for i in range(N):
     h=int(input())
     H.append(h)
-    if h not in m:
-        m.append(h)
+H.append(-1)
 
-# def count_ice(height):
-#     cnt=0
-#     switch=False
-#     arr=[max(0,elem-height) for elem in H]
-#     for i in range(N):
-#         if not switch and arr[i]!=0:
-#             cnt+=1
-#             switch=True
-#         elif arr[i]==0:
-#             switch=False
-#     return cnt
 
-def isHigh(a,b):
-    if a-b>0:
-        return 1
-    else:
-        return 0
-
-def count_ice(height):
-    cnt=isHigh(H[0],height)
-    arr=[cnt]
-    for i in range(1,N):
-        n=isHigh(H[i],height)
-        if arr[-1]!=n:
-            arr.append(n)
-            cnt+=n
-    return cnt
-
-ans=0
-for i in m:
-    ans=max(ans,count_ice(i))
-
+for i in range(1,len(H)):
+    mx_idx=H.index(max(H))
+    if H[mx_idx-1]!=0 and H[mx_idx+1]!=0:
+        cnt+=1
+    elif H[mx_idx-1]==0 and H[mx_idx+1]==0:
+        cnt-=1
+    # 방문 체크
+    H[mx_idx]=0
+    ans=max(cnt,ans)
+    
 print(ans)

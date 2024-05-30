6b802efbca57cc7889a51ade63d3183e94c4e1ad
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-30 16:43:25 +0900

    Title: Date to Date, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240530/Date to Date/date-to-date.py
+++ 240530/Date to Date/date-to-date.py
@@ -1,6 +1,19 @@
+# m1,d1,m2,d2=map(int,input().split())
+# days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+# start=sum(days[:m1])+d1
+# end=sum(days[:m2])+d2
+# print(end-start+1)
+
 m1,d1,m2,d2=map(int,input().split())
 days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
-start=sum(days[:m1])+d1
-end=sum(days[:m2])+d2
+cnt=1
+while True:
+    if m1==m2 and d1==d2:
+        break
+    d1+=1
+    cnt+=1
+    if d1==days[m1]:
+        d1=0
+        m1+=1
 
-print(end-start+1)
+print(cnt)

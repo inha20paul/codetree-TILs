ec5bcd00c8e91ecc6e4c196c3d24baa588f6ce24
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-05-30 16:33:42 +0900

    Title: Time to Time, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240530/Time to Time/time-to-time.py
+++ 240530/Time to Time/time-to-time.py
@@ -1,5 +1,19 @@
-a,b,c,d=map(int,input().split())
-start=(60*a)+b
-end=(60*c)+d
+# a,b,c,d=map(int,input().split())
+# start=(60*a)+b
+# end=(60*c)+d
 
-print(end-start)
+# print(end-start)
+
+a,b,c,d=map(int,input().split())
+cnt=0
+
+while True:
+    if a==c and b==d:
+        break
+    cnt+=1
+    b+=1
+    if b==60:
+        a+=1
+        b=0
+
+print(cnt)

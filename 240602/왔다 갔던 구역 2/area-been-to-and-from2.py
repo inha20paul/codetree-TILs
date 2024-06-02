ccafe82f31b936bcb87e1ea4b08acc766d0145e4
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-06-02 18:31:30 +0900

    Title: 왔다 갔던 구역 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240602/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240602/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -10,10 +10,22 @@ for i in range(n):
     x,D=input().split()
     x=int(x)
     for j in range(x):
-        current+=dirr[D]
-        if current in visited:
-            cnt+=1
-        visited.append(current)
+        if D=='R':
+            visited.append(current)
+            current+=dirr[D]
+        else:
+            current+=dirr[D]
+            visited.append(current)
 
 
-print(cnt)
+arr2=[0]*(max(visited)-min(visited)+1)
+isVisit=[False]*(max(visited)-min(visited)+1)
+num=min(visited)
+
+for elem in visited:
+    idx=elem-num
+    if isVisit[idx]:
+        arr2[idx]=1
+    isVisit[idx]=True
+
+print(sum(arr2))

a9893acd755f53487b244c342803c970a77fb4fa
inha20paul <156832213+inha20paul@users.noreply.github.com>
2024-06-25 01:48:49 +0900

    Title: 흰검 칠하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,44 @@
+# 명령 수
+n=int(input())
+
+dirr={
+    'L':-1,
+    'R':1
+}
+
+arr=[0]
+curr=0
+
+for i in range(n):
+    x,d=input().split()
+    x=int(x)
+    curr+=(x-1)*dirr[d]
+    arr.append(curr)
+
+size=max(arr)-min(arr)+1
+
+gray=[0]*size
+bw=[0]*size
+g=0
+for j in range(len(arr)-1):
+    start,end=arr[j]-min(arr),arr[j+1]-min(arr)
+    if start<end:
+        for k in range(start,end+1):
+            bw[k]=1
+            gray[k]+=1
+            
+    else:
+        for k in range(end,start+1):
+            bw[k]=0
+            gray[k]+=10
+
+
+for i in range(len(gray)):
+    if gray[i]%10>=2 and gray[i]>=20:
+        bw[i]=0
+        g+=1
+
+b=sum(bw)
+w=size-b-g
+
+print(g,b,w)
